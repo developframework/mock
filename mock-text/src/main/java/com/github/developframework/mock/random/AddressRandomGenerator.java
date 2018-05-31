e1151d00b71f0cef9763af95d2c3106a2b6ff5f2
qiushui <408000511@qq.com>
2018-05-31 11:03:41 +0800

    加入省份城市限制

--- mock-text/src/main/java/com/github/developframework/mock/random/AddressRandomGenerator.java
+++ mock-text/src/main/java/com/github/developframework/mock/random/AddressRandomGenerator.java
@@ -1,13 +1,17 @@
 package com.github.developframework.mock.random;
 
 import com.github.developframework.mock.MockCache;
+import com.github.developframework.mock.MockException;
 import com.github.developframework.mock.MockPlaceholder;
 import com.github.developframework.mock.RandomGeneratorRegistry;
 import com.github.developframework.regiondata.ChinaRegionProvider;
+import com.github.developframework.regiondata.City;
 import com.github.developframework.regiondata.County;
+import com.github.developframework.regiondata.Province;
 import org.apache.commons.lang3.RandomUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 随机地址生成器
@@ -18,17 +22,37 @@ import java.util.List;
 public class AddressRandomGenerator implements RandomGenerator<County>{
 
     private static final String PARAMETER_LEVEL = "level";
+    private static final String PARAMETER_PROVINCE = "province";
+    private static final String PARAMETER_CITY = "city";
 
     private static ChinaRegionProvider chinaRegionProvider = new ChinaRegionProvider();
 
-    private List<County> counties;
-
-    public AddressRandomGenerator() {
-        counties = chinaRegionProvider.getChina().getAllCounties();
-    }
-
     @Override
     public County randomValue(RandomGeneratorRegistry randomGeneratorRegistry, MockPlaceholder mockPlaceholder, MockCache mockCache) {
+        List<County> counties;
+
+        Optional<String> provinceOptional = mockPlaceholder.getParameter(PARAMETER_PROVINCE, String.class);
+
+        if (provinceOptional.isPresent()) {
+            Optional<Province> provinceByName = chinaRegionProvider.getChina().getProvinceByName(provinceOptional.get());
+            if(provinceByName.isPresent()) {
+                Optional<String> cityOptional = mockPlaceholder.getParameter(PARAMETER_CITY, String.class);
+                if(cityOptional.isPresent()) {
+                    Optional<City> cityByName = provinceByName.get().getCityByName(cityOptional.get());
+                    if(cityByName.isPresent()) {
+                        counties = cityByName.get().getAllCounties();
+                    } else {
+                        throw new MockException("address random generator city attribute \"%s\" value is not exist.", cityOptional.get());
+                    }
+                } else {
+                    counties = provinceByName.get().getAllCounties();
+                }
+            } else {
+                throw new MockException("address random generator province attribute \"%s\" value is not exist.", provinceOptional.get());
+            }
+        } else {
+            counties = chinaRegionProvider.getChina().getAllCounties();
+        }
         return counties.get(RandomUtils.nextInt(0, counties.size()));
     }
 
